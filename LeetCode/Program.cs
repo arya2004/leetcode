31bd73d21f65b07fddf962a9cb24ede62dbef556
arya2004 <arya20j4@gmail.com>
2023-07-04 01:31:18 +0530

    fib and string pal

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -6,8 +6,12 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            IsPalindrome isPalindrome = new IsPalindrome();
-            Console.WriteLine("{0}", isPalindrome.main(1221));
+           // IsPalindrome isPalindrome = new IsPalindrome();
+            //Console.WriteLine("{0}", isPalindrome.main(1221));
+            string a = "A man, a plan, a canal: Panama";
+            Fibonacci fibonacci = new Fibonacci();
+
+            Console.WriteLine("{0}", fibonacci.Main(10));
         }
     }
 }
