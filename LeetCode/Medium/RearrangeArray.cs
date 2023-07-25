34f7feb417647dc2716d01ea65a0135a90892a2e
arya2004 <arya20j4@gmail.com>
2023-07-26 00:54:31 +0530

    max profit, sort color, rearrange arr

---
+++ LeetCode/Medium/RearrangeArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium
+{
+    internal class RearrangeArray
+    {
+        public int[] main(int[] nums)
+        {
+
+        }
+    }
+}
