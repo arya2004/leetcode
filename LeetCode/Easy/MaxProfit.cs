6733fb93644c7050b72881a4418db8414c4b24bf
arya2004 <arya20j4@gmail.com>
2023-07-26 02:12:29 +0530

    added maxprofit

--- LeetCode/Easy/MaxProfit.cs
+++ LeetCode/Easy/MaxProfit.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace LeetCode.Easy
 {
-    internal class MaxProfit
+    public class MaxProfit
     {
+        public int main(int[] prices)
+        {
+            int minimal = prices[0];
+            int maxprofot = int.MinValue;
+            for (int i = 0; i < prices.Length; i++)
+            {
+                minimal = Math.Min(minimal, prices[i]);
+                int temp = prices[i] - minimal;
+                maxprofot = Math.Max(maxprofot, temp);
+            }
+            return maxprofot;
+        }
     }
 }
