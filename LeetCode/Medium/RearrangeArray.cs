6b7bb7a545a24498ef848d69119612c8c5fbcd1b
arya2004 <arya20j4@gmail.com>
2023-07-26 02:12:01 +0530

    next permutation and rearrange array

--- LeetCode/Medium/RearrangeArray.cs
+++ LeetCode/Medium/RearrangeArray.cs
@@ -10,7 +10,24 @@ namespace LeetCode.Medium
     {
         public int[] main(int[] nums)
         {
+            int odd = 1;
+            int even = 0;
+            int[] ints = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                {
+                    ints[even] = nums[i];
+                    even = even + 2;
+                }
+                if (nums[i] < 0)
+                {
+                    ints[odd] = nums[i];
+                    odd = odd + 2;
+                }
 
+            }
+            return ints;
         }
     }
 }
