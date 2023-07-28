27e8439307a3eff89f735858612f5aeb40912124
arya2004 <arya20j4@gmail.com>
2023-07-29 02:46:48 +0530

    medium prblems solved

---
+++ LeetCode/Medium/FindTriangularSumofanArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Medium
+{
+    public class FindTriangularSumofanArray
+    {   
+        //incoplete
+        public int TriangularSum(int[] nums)
+        {   
+            
+            //int baseNum = 1;
+            //int numToReturn = nums[0];
+            //for (int col = 1; col <= nums.Length; col++)
+            //{
+            //    numToReturn = (numToReturn + (baseNum * nums[col])) % 10;
+            //    baseNum = baseNum *(nums.Length - col)/ col;
+            //}
+            //return numToReturn;
+        }
+    }
+}
