4cb68b9c665943d59e01bc60dace33e673343e76
arya2004 <arya20j4@gmail.com>
2023-07-31 02:30:42 +0530

    switched to cpp

---
+++ LeetCodeinCPP/LeetCodeinCPP.cpp
@@ -0,0 +1,42 @@
+// LeetCodeinCPP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <deque>
+#include <forward_list>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+#include "Pow(x, n).cpp"
+
+using namespace std;
+
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+    }
+};
+int main()
+{   
+    
+    Solution s;
+    
+}
+
