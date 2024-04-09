ad7790c4c807c35d01687979afc2f665899cff2b
arya2004 <arya20j4@gmail.com>
2024-04-10 02:19:09 +0530

    feat: 3sum and pascal

---
+++ sheet/array/IIISum.java
@@ -0,0 +1,76 @@
+package sheet.array;
+
+import java.util.*;
+
+public class IIISum {
+
+    
+    //Time complexity: N^2
+    public List<List<Integer>> threeSumBrute(int[] arr) {
+        int n = arr.length;
+        Set<List<Integer>> st = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> hashset = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+         
+                int third = -(arr[i] + arr[j]);
+
+                if (hashset.contains(third)) {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+                    temp.sort(null);
+                    st.add(temp);
+                }
+                hashset.add(arr[j]);
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>(st);
+        return ans;
+    }
+
+    //sorted and 2 pointer
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int j = i + 1;
+            int k = n -1;
+            while (j < k) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum < 0){
+                    j++;
+                }else if(sum > 0){
+                    k--;
+                }else{
+                    List<Integer> temp =  Arrays.asList(nums[i], nums[j], nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
+                                        
+
+
+                }
+
+                
+            }
+
+
+        }
+
+
+
+        return ans;
+
+    }
+}
---
+++ sheet/array/pascalTriangle.java
@@ -0,0 +1,28 @@
+package sheet.array;
+
+
+import java.util.*;
+
+public class pascalTriangle {
+    public static List<Integer> generateRow(int row) {
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+
+    public static List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+      
+        for (int row = 1; row <= n; row++) {
+            ans.add(generateRow(row));
+        }
+        return ans;
+    }
+}
